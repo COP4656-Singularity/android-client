0bfdfa41875bc2e5258205724168a953fee09679
cnj15b <cnj15b@my.fsu.edu>
2016-07-27 20:29:50 -0500

    Added missing libraries to Note Detail

--- app/src/main/java/singularity/twodolist/NoteDetail.java
+++ app/src/main/java/singularity/twodolist/NoteDetail.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import org.json.JSONException;
+import android.widget.Toast;
 
 import org.json.JSONObject;
 
